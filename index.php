9177b9e311ab3da3c75d82d559e0ef781eb51062
Mathew Yarbrough <Mathewyar@gmail.com>
2017-09-07 12:36:58 -0700

    Age Check

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 
 $user = "rob";
+$age = "25";
 
 if ($user == "rob"){
 	echo "Hello Rob!";
 	} else {
-		echo "Who are you";
+			echo "Who are you";
 		}
+	echo <br><br>
 	
-	
+if ($age >= 18){
+	echo "You May Enter"
+    } else {
+		echo "You Shall Not Pass!!!"		
+		}
 	
 
 
